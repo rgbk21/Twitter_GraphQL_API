664b5a938e1dd99fa3fac77a10d0692df7b3a914
rgbk21 <rajgaurav.bk@gmail.com>
2020-05-31 11:59:21 -0400

    Context Loads

---
+++ src/main/java/com/example/twitterAPI/GraphQLSchemaConfig.java
@@ -0,0 +1,70 @@
+package com.example.twitterAPI;
+
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+@Component
+public class GraphQLSchemaConfig {
+
+    @Bean
+    public String schemaSource(@Value("${graphql.schemaLocation}") String schemaLocation) throws Exception{
+        return getResourceFileAsString(schemaLocation);
+    }
+
+    @Bean
+    public GraphQLSchema buildGraphQLSchema(String schemaSource, GraphQLDataFetchers graphQLDataFetchers){
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schemaSource);
+
+        RuntimeWiring runtimeWiring = buildWiring(graphQLDataFetchers);
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring(GraphQLDataFetchers graphQLDataFetchers) {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+                .build();
+    }
+
+
+    //https://stackoverflow.com/questions/676250/different-ways-of-loading-a-file-as-an-inputstream
+    //https://stackoverflow.com/questions/6068197/utils-to-read-resource-text-file-to-string-java
+    /**
+     * Reads given resource file as a string.
+     *
+     * @param fileName path to the resource file
+     * @return the file's contents
+     * @throws IOException if read fails for any reason
+     */
+    private String getResourceFileAsString(String fileName) throws IOException {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try (InputStream is = classLoader.getResourceAsStream(fileName)) {
+            if (is == null) return null;
+            try (InputStreamReader isr = new InputStreamReader(is);
+                 BufferedReader reader = new BufferedReader(isr)) {
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
+        }
+    }
+
+
+}
